96ca19a93678df5a936d3357d3b5dc81e50d9446
Jeffrey <159982332+Jefftheotherdeveloper@users.noreply.github.com>
2026-04-07 09:34:46 -0400

    Add files via upload

---
+++ LogIn.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class LogIn{
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("----------------------\n");
+        Scanner scanner = new Scanner(System.in);
+        boolean userANDpass = true;
+
+        while(userANDpass)
+        {
+        System.out.println("\n- Cooper & Rocco's Warehouse -\n");
+        System.out.print("Username: ");
+        String input_username = scanner.nextLine();
+        System.out.print("Password: ");
+        String input_password = scanner.nextLine();
+
+        if(input_username.equals("MMiller-001") && input_password.equals("MM1-123abc")){
+            System.out.println("\nSuccesful login\n");
+
+            //Creates the employee(User)
+            Employee printEmployee = new Employee("Micheal", "Miller", 
+            "123-456-7890", "Micheal05Miller@gmail.com", input_username, 
+            input_password, 123456, "MMiller001@company.com", "CEO");
+            
+            //Sends user to CenterHub after logging in 
+            CenterHub.main(printEmployee);
+            userANDpass = false;
+            break;
+        }
+        else
+        {
+            System.out.println("\nInvalid credentials");
+        }
+        }
+        scanner.close();
+    }
+}
